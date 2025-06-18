3249306e9f9a86601e0334b4cc67f69ea6e32b5b
Дмитрий Бажаев <48925297+qb-wht@users.noreply.github.com>
2025-06-18 19:05:06 +0300

    Add image view editor

--- client/src/app/viewEditor/ui/ViewEditor.tsx
+++ client/src/app/viewEditor/ui/ViewEditor.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import f from "./view.png";
+
 export const ViewEditor = () => {
   return (
     <div className="center">
-      <h1>View Editor</h1>
+      <Image
+        style={{ width: "60rem", height: "40rem" }}
+        src={f}
+        alt="View Editor"
+      />
     </div>
   );
 };
