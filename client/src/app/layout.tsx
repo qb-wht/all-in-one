12b931fea660d5c2858a435819f8cc53e51e86fb
Дмитрий Бажаев <48925297+qb-wht@users.noreply.github.com>
2025-06-21 10:10:11 +0300

    Fix style folder name

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
-import '@/shared/styles/index.css';
+import '@/4_shared/styles/index.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
