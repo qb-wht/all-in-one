023af075a1ba7d48a35a60dbd9bbe8cbd268972d
Дмитрий Бажаев <48925297+qb-wht@users.noreply.github.com>
2025-07-05 16:19:24 +0300

    Add view editor state, styles

--- client/src/widgets/appSettingsBar/ui/AppSettingsBar.tsx
+++ client/src/widgets/appSettingsBar/ui/AppSettingsBar.tsx
@@ -2,12 +2,17 @@ import {Button} from 'antd';
 import {useAppSettingsStore} from '@/entities/appSettings';
 import {cn} from '@/shared/lib/classNames';
 import type {PropsOf} from '@/shared/types';
+import s from './AppSettingsBar.module.css';
 
 export type AppSettingsBarProps = {} & PropsOf<HTMLDivElement>;
 
 export const AppSettingsBar = (props: AppSettingsBarProps) => {
   const {className, ...anotherProps} = props;
-  const classNames = cn('row g-1 p-1 jc-space-between', className).build();
+  const classNames = cn(
+    'row jc-space-between ai-center g-1 pr-1 pl-1',
+    s.appSettingsBar,
+    className,
+  ).build();
 
   const isLocal = useAppSettingsStore((state) => state.isLocal);
   const changeIsLocal = useAppSettingsStore((state) => state.changeIsLocal);
--- client/src/widgets/viewEditor/ui/ViewEditor.tsx
+++ client/src/widgets/viewEditor/ui/ViewEditor.tsx
@@ -1,9 +1,35 @@
+import {useEffect, useState} from 'react';
+import {useEditorStore} from '@/entities/editor';
+import {cn} from '@/shared/lib/classNames';
+import type {PropsOf} from '@/shared/types';
 import image from './view.png';
 
-export const ViewEditor = () => {
+export type ViewEditorProps = {} & PropsOf<HTMLDivElement>;
+
+export const ViewEditor = (props: ViewEditorProps) => {
+  const {className, ...anotherProps} = props;
+  const classNames = cn('center', className).build();
+  const openedFile = useEditorStore((state) => state.openedFile);
+
+  const [localValue, setLocalValue] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (openedFile) {
+      openedFile?.content.text().then((value) => setLocalValue(value));
+      return;
+    }
+
+    setLocalValue(undefined);
+  }, [openedFile]);
+
   return (
-    <div className='center'>
-      <img style={{width: '60rem', height: '40rem'}} src={image} alt='View Editor' />
+    <div className={classNames} {...anotherProps}>
+      {localValue !== undefined && (
+        <div className='center fd-column'>
+          {openedFile?.path}
+          <img style={{width: '54rem', height: '36rem'}} src={image} alt='View Editor' />
+        </div>
+      )}
     </div>
   );
 };
